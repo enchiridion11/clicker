0f5e561fdb0ff792a4769b31e7bfc2ec7ef599eb
Michael <michaelr@thoopid.com>
2018-05-25 16:50:25 +0200

    possible data editor variable storing

---
+++ Assets/Code/CameraShakePresentList.cs
@@ -0,0 +1,7 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShakePresentList : MonoBehaviour {
+	public List<CameraShakePreset> list = new List<CameraShakePreset>();
+}
